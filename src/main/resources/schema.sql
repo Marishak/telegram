29d34407f93bebceeab66ce83cc63ec572e756f1
Bekishchuk Marina <bekishchukmm@synergyteam.msk.ru>
2025-05-29 00:20:03 +0300

    Исправлен тип данных для id таблиц оО

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,14 +1,14 @@
 CREATE TABLE IF NOT EXISTS "users" (
-        id SERIAL PRIMARY KEY,
+        id BIGSERIAL PRIMARY KEY,
         first_name VARCHAR(50),
         last_name VARCHAR(50),
         username VARCHAR(50) UNIQUE,
-        user_id SERIAL NOT NULL UNIQUE
+        user_id BIGINT NOT NULL UNIQUE
     );
 
 CREATE TABLE IF NOT EXISTS message (
-    id SERIAL PRIMARY KEY,
-    user_id INT NOT NULL,
+    id BIGSERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL,
     message TEXT NOT NULL,
     create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
